5e1dfc5cf9f812a45f96d80c13678ad97bd6e472
Stefano Sega <stefano.sega-1@studenti.unitn.it>
2019-04-10 21:05:39 +0200

    Implemented different visualization

--- django_netjsongraph/base/snapshot.py
+++ django_netjsongraph/base/snapshot.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 from .base import TimeStampedEditableModel
+from .topology import SNAPSHOT_KINDS
 
 
 class AbstractSnapshot(TimeStampedEditableModel):
@@ -12,6 +13,11 @@ class AbstractSnapshot(TimeStampedEditableModel):
                                  on_delete=models.CASCADE)
     data = models.TextField(blank=False)
     date = models.DateField(auto_now=True)
+    kind = models.CharField(_('kind'),
+                            max_length=16,
+                            choices=SNAPSHOT_KINDS,
+                            default=SNAPSHOT_KINDS[0],
+                            db_index=True)
 
     class Meta:
         verbose_name_plural = _('snapshots')
--- django_netjsongraph/static/netjsongraph/js/topology-history.js
+++ django_netjsongraph/static/netjsongraph/js/topology-history.js
@@ -3,17 +3,23 @@ window.initTopologyHistory = function ($) {
     var datepicker = $('#njg-datepicker'),
         today = new Date(),
         apiUrl = datepicker.attr('data-history-api');
+    var kindpicker=$("input[name='visualization_kind']");
+    var kind = 'normal';
+    kindpicker.change(function() {;
+        kind = this.value
+    });
     today.setHours(0, 0, 0, 0);
     datepicker.datepicker({dateFormat: 'dd/mm/yy'});
     datepicker.datepicker('setDate', today);
     datepicker.change(function () {
         var date = datepicker.val().split('/').reverse().join('-'),
-            url = apiUrl + '?date=' + date;
+            url = apiUrl + '?kind=' + kind + '&date=' + date;
         // load latest data when looking currentDate
         if (datepicker.datepicker('getDate').getTime() === today.getTime()) {
             url = window.__njg_default_url__;
         }
-        $.getJSON(url).done(function (data) {
+        // load latest data when looking currentDate
+        $.getJSON(url).done(function(data) {
             window.graph = window.loadNetJsonGraph(data);
         }).error(function (xhr) {
             alert(xhr.responseJSON.detail);
