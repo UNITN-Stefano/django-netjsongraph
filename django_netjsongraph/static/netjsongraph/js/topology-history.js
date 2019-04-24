0adbdc1af9c1e23537e7c6aef5881ee06a80317b
Stefano Sega <stefano.sega-1@studenti.unitn.it>
2019-04-24 18:26:41 +0200

    Fixed and formatted code

--- django_netjsongraph/static/netjsongraph/js/topology-history.js
+++ django_netjsongraph/static/netjsongraph/js/topology-history.js
@@ -3,13 +3,13 @@ window.initTopologyHistory = function ($) {
     var datepicker = $('#njg-datepicker'),
         today = new Date(),
         apiUrl = datepicker.attr('data-history-api');
-    var kindpicker=$("input[name='visualization_kind']");
+    var kindpicker = $("input[name='visualization_kind']");
     var kind = 'normal';
-    kindpicker.change(function() {;
-        kind = this.value
+    kindpicker.change(function () {
+        kind = this.value;
     });
     today.setHours(0, 0, 0, 0);
-    datepicker.datepicker({dateFormat: 'dd/mm/yy'});
+    datepicker.datepicker({ dateFormat: 'dd/mm/yy' });
     datepicker.datepicker('setDate', today);
     datepicker.change(function () {
         var date = datepicker.val().split('/').reverse().join('-'),
@@ -19,7 +19,7 @@ window.initTopologyHistory = function ($) {
             url = window.__njg_default_url__;
         }
         // load latest data when looking currentDate
-        $.getJSON(url).done(function(data) {
+        $.getJSON(url).done(function (data) {
             window.graph = window.loadNetJsonGraph(data);
         }).error(function (xhr) {
             alert(xhr.responseJSON.detail);
